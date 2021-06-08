996539b288b593f38dbafee72d697d989c79c747
nguyenduyhoc <duyhocnguyen97@gmail.com>
2021-06-08 13:48:47 +0700

    Finally

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,8 +1,11 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'somedeployedURL'
-export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern'
-export const POST_LOADED_SUCCESS = 'POST_LOADED_SUCCESS'
-export const POST_LOADED_FAIL = 'POST_LOADED_FAIL'
-export const ADD_POST = 'ADD_POST'
-export const DELETE_POST = 'DELETE_POST'
-export const UPDATE_POST = 'UPDATE_POST'
-export const FIND_BOX = 'FIND_BOX'
+export const apiUrl =
+  process.env.NODE_ENV !== "production"
+    ? "https://glacial-anchorage-08342.herokuapp.com/api"
+    : "somedeployedURL";
+export const LOCAL_STORAGE_TOKEN_NAME = "learnit-mern";
+export const POST_LOADED_SUCCESS = "POST_LOADED_SUCCESS";
+export const POST_LOADED_FAIL = "POST_LOADED_FAIL";
+export const ADD_POST = "ADD_POST";
+export const DELETE_POST = "DELETE_POST";
+export const UPDATE_POST = "UPDATE_POST";
+export const FIND_BOX = "FIND_BOX";
